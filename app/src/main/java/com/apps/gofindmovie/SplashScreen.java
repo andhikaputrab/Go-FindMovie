b1d56ccf18c51057ca1c9bc1f7e1554bb7ef09a5
andhikaputrab <andhikaputra1301@gmail.com>
2020-05-26 22:38:29 +0700

    Menambahkan SplashScreen

---
+++ app/src/main/java/com/apps/gofindmovie/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.apps.gofindmovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 5000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView logo;
+    TextView descLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animation
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo = findViewById(R.id.logo);
+        descLogo = findViewById(R.id.descLogo);
+
+        logo.setAnimation(topAnimation);
+        descLogo.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, IntroScreen.class));
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
